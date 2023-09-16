8e68903da94e96693d1bec17b0291b914a2a3e36
RichardMathews <richardmathewsa.silva@hotmail.com>
2023-09-16 20:40:01 -0300

    finalizando projeto

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -1,26 +1,26 @@
 import { FastifyInstance } from "fastify";
-import { createReadStream } from 'node:fs'
-import { z } from 'zod'
-import { prisma } from '../lib/prisma'
-import { openai } from '../lib/openai'
+import { createReadStream } from "node:fs";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
-  app.post('/videos/:videoId/transcription', async (request) => {
+  app.post('/videos/:videoId/transcription', async (req) => {
     const paramsSchema = z.object({
-      videoId: z.string().uuid()
+      videoId: z.string().uuid(),
     })
 
-    const { videoId } = paramsSchema.parse(request.params)
+    const { videoId } = paramsSchema.parse(req.params)
 
     const bodySchema = z.object({
-      prompt: z.string()
+      prompt: z.string(),
     })
 
-    const { prompt } = bodySchema.parse(request.body)
+    const { prompt } = bodySchema.parse(req.body)
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
-        id: videoId
+        id: videoId,
       }
     })
 
@@ -33,20 +33,22 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
       language: 'pt',
       response_format: 'json',
       temperature: 0,
-      prompt
+      prompt,
     })
 
     const transcription = response.text
 
     await prisma.video.update({
       where: {
-        id: videoId
+        id: videoId,
       },
       data: {
-        transcription: transcription
+        transcription,
       }
     })
 
-    return { transcription }
+    return {
+      transcription,
+    }
   })
 }
--- src/routes/get-all-prompts.ts
+++ src/routes/get-all-prompts.ts
@@ -1,10 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from '../lib/prisma'
+import 'dotenv/config'
 
 export async function getAllPromptsRoute(app: FastifyInstance) {
   app.get('/prompts', async () => {
     const prompts = await prisma.prompt.findMany()
-    
+    console.log(process.env.OPENAI_KEY)
     return prompts
   })
 }
